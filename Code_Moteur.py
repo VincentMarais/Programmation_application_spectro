5d773a2fcb38b7bf3ae0673bd435fed6722f1501
Vincent Marais <vincent.travail12@gmail.com>
2023-03-10 17:34:41 +0100

    Moteur V2

--- Code_Moteur.py
+++ Code_Moteur.py
@@ -1,13 +1,14 @@
 import serial  
 import time 
 
-s = serial.Serial('COM5',115200)
+s = serial.Serial('COM5', 115200)
 
-s.write("\r\n\r\n")
+s.write("\r\n\r\n".encode()) # encode pour convertir "\r\n\r\n" 
 time.sleep(2)   # Wait for grbl to initialize 
 s.flushInput()  # Flush startup text in serial input
 
 def deplacement():
-        s.write("G0 X200" + '\n')
+        gcode= 'G0 X50' + '\n'
+        s.write(gcode.encode())
 
 # Wait here until grbl is finished to close serial port and file.
